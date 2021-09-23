2d22efb2860ee9dd1e8b658ec6499bbe51ecc8b5
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-09-23 17:52:15 +0530

    Added Weather Icons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App</h1>
+      <i className="wi wi-rain" style={{fontSize:"75px"}}></i>
     </div>
   )
 }
